df378064572fbbeff0dae49b371ce28f5d57aa75
Vit <vit@email.com>
2024-10-30 20:53:51 +0000

    Create Tsv file loader class which opens the file, reads the content and create a title instance for each line of data.

---
+++ src/main/java/software/ulpgc/kata2/TsvFileTitleLoader.java
@@ -0,0 +1,58 @@
+package software.ulpgc.kata2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+public class TsvFileTitleLoader implements TsvFileLoader {
+    private final File file;
+
+    public TsvFileTitleLoader(File file) {
+        this.file = file;
+    }
+
+    public static TsvFileLoader with(File file) {return new TsvFileTitleLoader(file);}
+
+    @Override
+    public List<Title> load() {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            return load(br);
+        } catch (IOException e) {
+            return Collections.emptyList();
+        }
+    }
+
+    private List<Title> load(BufferedReader br) throws IOException {
+        List<Title> titles = new ArrayList<Title>();
+        br.readLine();
+
+        while (true) {
+            String line = br.readLine();
+            if(line == null) break;
+            titles.add(toTitle(line));
+        }
+        return titles;
+    }
+
+    private Title toTitle(String line) {
+        return toTitle(line.split("\t"));
+    }
+
+    private Title toTitle(String[] split) {
+        return new Title(
+                split[0],
+                split[1],
+                split[2],
+                split[3],
+                split[4].equals("1"),
+                split[5],
+                split[6],
+                split[7],
+                split[8].split(",")
+        );
+    }
+}
