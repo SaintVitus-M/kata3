ec16434de8a7231c978644b09c5dbefac04a3b23
Vit Mikula <vit.mikula101@alu.ulpgc.es>
2024-11-06 16:42:25 +0000

    Update Main.java

--- src/main/java/software/ulpgc/kata3/apps/windows/Main.java
+++ src/main/java/software/ulpgc/kata3/apps/windows/Main.java
@@ -13,24 +13,28 @@ import java.io.File;
 import java.util.List;
 import java.util.Map;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("Introduzca la ruta del archivo tsv");
             return;
         }
-
+        // Load the TSV file path from the first argument.
         String tsvPath = args[0];
 
+        // Load all the title objects from the TSV file into a list. 
         List<Title> titles = TsvFileTitleLoader.with(new File(tsvPath)).load();
 
+        // Calculate two different statistics from title list data.
         TitleStatistic tStat = StatisticFactory.getStatistic(StatisticType.TIMESERIES);
         TitleStatistic cStat = StatisticFactory.getStatistic(StatisticType.CATEGORY);
 
+        // Load the statistics respectively into charts. 
         ChartLoader loader = new TitleChartLoader(tStat.calculate(titles), cStat.calculate(titles));
 
+        // Create the MainFrame where the chart will be visualized.
+        // Insert selector component into the MainFrame.
+        // Load a chart selected from the selector component.
         MainFrame mainFrame = new MainFrame();
         mainFrame.put("select", new SelectStatisticCommand(mainFrame.selectStatisticDialog(), loader, mainFrame.chartDisplay()));
         mainFrame.chartDisplay().show(loader.load(0));
